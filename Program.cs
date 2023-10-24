ee41877fde762ac93454b24d35faeca1658a8d7e
Abdulaziz <you@example.com>
2023-10-24 22:13:07 +0500

    First C# project

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Raqamlar sonini kiriting:");
+int raqamlar_soni = Convert.ToInt32(Console.ReadLine());
+double summa = 0;
+ Console.WriteLine("Sonlarni kiriting:");
+for (int birinchi_son = 0;birinchi_son < raqamlar_soni;birinchi_son++){
+    Console.Write(">> ");
+    summa += Convert.ToSingle(Console.ReadLine());
+}
+Console.WriteLine("Kiritilgan raqamlar o'rta arifmetigi " + summa/raqamlar_soni);
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
+
 # Averagely
 This is my first C# project 
+
+
+
+
+## 🔗 Links
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/abdulaziz-najmiddinov-257479204/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BQ9Z%2BLWVJTIOPNwmtiGA68Q%3D%3D)
+
+
+## Authors
+
+- [@octokatherine](https://www.github.com/Coder200709)
+
